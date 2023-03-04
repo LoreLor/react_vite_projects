708c36ddcaf4122f2e77202b653c1e0613f85da5
Lorena De Armas <lorenadearmas01@gmail.com>
2023-03-04 18:08:16 -0300

    standard.js

--- juego-ta-te-ti/src/App.jsx
+++ juego-ta-te-ti/src/App.jsx
@@ -1,60 +1,54 @@
 import { useState } from 'react'
-import confetti from 'canvas-confetti';
+import confetti from 'canvas-confetti'
 
-import { TURNS } from './utils/constants';
-import { checkWinnerFrom } from './utils/checkWinner';
-import { checkEndGame } from './utils/checkEndGame';
-import WinnerModal from './components/WinnerModal';
+import { TURNS } from './utils/constants'
+import { checkWinnerFrom } from './utils/checkWinner'
+import { checkEndGame } from './utils/checkEndGame'
+import WinnerModal from './components/WinnerModal'
 
 import './App.css'
-import Board from './components/Board';
-import TurnsComponent from './components/TurnsComponent';
-import { resetGameStorage, saveGameToStorage } from './utils/storage';
+import Board from './components/Board'
+import TurnsComponent from './components/TurnsComponent'
+import { resetGameStorage, saveGameToStorage } from './utils/storage'
 
-
-
-function App() {
-
-  //tablero de 9 posiciones y fill() metodo para rellenar
+function App () {
+  // tablero de 9 posiciones y fill() metodo para rellenar
   const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board');
-    return boardFromStorage? JSON.parse(boardFromStorage) : Array(9).fill(null)
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
   })
 
-
-  //para saber de quien es el turno
+  // para saber de quien es el turno
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn');
-    return turnFromStorage? JSON.parse(turnFromStorage) : TURNS.X 
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X
   })
 
+  // para saber cuando hay un ganador
+  const [winner, setWinner] = useState(null) // null=hay ganador, falso= empate
 
-  //para saber cuando hay un ganador
-  const [winner, setWinner] = useState(null)  //null=hay ganador, falso= empate
-
-
-  //cambia turno cada vez que se selecciona uno
+  // cambia turno cada vez que se selecciona uno
   const updateBoard = (index) => {
-    //para evitar modificar si ya hay algo en la posicion ->se mantiene como esta
-    if (board[index] || winner) return;
+    // para evitar modificar si ya hay algo en la posicion ->se mantiene como esta
+    if (board[index] || winner) return
 
-    //para no modificar el estado copio uno y luego actualizo con los 
-    //nuevos datos
+    // para no modificar el estado copio uno y luego actualizo con los
+    // nuevos datos
     const newBoard = [...board]
-    newBoard[index] = turn //x | o
+    newBoard[index] = turn // x | o
     setBoard(newBoard)
 
-    //cambia turno
+    // cambia turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    //persisto estado del tablero
+    // persisto estado del tablero
     saveGameToStorage({
       board: newBoard,
       turn: newTurn
     })
-    
-    //chequeo si existe un ganador
+
+    // chequeo si existe un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti()
@@ -64,7 +58,7 @@ function App() {
     }
   }
 
-  //seteo los estados a iniciales
+  // seteo los estados a iniciales
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
@@ -73,23 +67,22 @@ function App() {
     resetGameStorage()
   }
 
-
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Empezar de Nuevo</button>
-      <Board 
-        board={board} 
-        updateBoard={updateBoard} 
+      <Board
+        board={board}
+        updateBoard={updateBoard}
       />
-      <TurnsComponent 
-        turn={turn}      
+      <TurnsComponent
+        turn={turn}
       />
 
       {/* modal para que muestre quien gano */}
-      <WinnerModal 
-        winner={winner} 
-        resetGame={resetGame}        
+      <WinnerModal
+        winner={winner}
+        resetGame={resetGame}
       />
     </main>
   )
