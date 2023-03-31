4c1b91ffc7063b55f77b4dc36c39f9119807f8b7
Lorena De Armas <lorenadearmas01@gmail.com>
2023-03-31 17:33:14 -0300

    google-translate-clone

--- clone-udemy-courses/src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-
-import CartPage from './CartPage'
-import CoursesPage from './CoursesPage'
-import HomePage from './HomePage'
-import SingleCoursePage from './SingleCoursePage'
-
-export { CartPage, CoursesPage, HomePage, SingleCoursePage }
